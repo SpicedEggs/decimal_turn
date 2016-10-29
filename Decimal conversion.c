0ac4730f1d7f4dd7ae9dc583439135836b8247c9
SpicedEggs <sinsinxiao@foxmail.com>
2016-10-29 21:54:09 +0800

    decimal to octal and hex
    decimal to octal and hex

---
+++ Decimal conversion.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+
+void turn(int x);
+int main()
+{
+	int x;
+	printf("please input a decimal number:\n");
+	scanf("%d",&x);
+	if(x<0)
+	{
+		printf("Are you fucking kidding me?why you sucker must input a negative number ?");
+		return 0; 
+	}
+	turn(x);
+	return 0; 
+}
+
+void turn(int x)
+{
+	int a[100]={0};
+	int b[100]={0};
+	int c[100]={0};
+	int al=0,bl=0,cl=0;
+	
+	int n=x;
+	int i=0;
+	while(n!=1)
+	{
+		a[i++]=n%2;
+		n/=2;
+	}
+	a[i]=1;
+	al=i;
+	printf("binary:\n")
+	for(i=al;i>=0;i--)//>=0 because al from 1 to a.length
+	{
+		printf("%d",a[i]);
+	}
+	printf("\n");
+	
+	
+	i=0;
+	while(al>=i){
+		if(al-i>=2)
+		{
+			b[bl++]+=a[i]*1+a[i+1]*2+a[i+2]*4;
+			i+=3;
+		}
+		else if (al-i>=1)
+		{
+			b[bl++]+=a[i]*1+a[i+1]*2;
+			i+=2;
+		}
+		else if (al-i>=0)
+		{
+			b[bl++]+=a[i]*1;
+			i+=1;
+		}
+	}
+	bl-=1;//bl-1 due to b[bl++] makes bl more than the real b.length
+	printf("octal:\n");
+	for(i=bl;i>=0;i--)
+	{
+		printf("%d",b[i]);
+	}
+	printf("\n");
+	
+	i=0;
+	while(al>=i){
+		if(al-i>=3)
+		{
+			c[cl++]+=a[i]*1+a[i+1]*2+a[i+2]*4+a[i+3]*8;
+			i+=4;
+		}
+		else if(al-i>=2)
+		{
+			c[cl++]+=a[i]*1+a[i+1]*2+a[i+2]*4;
+			i+=3;
+		}
+		else if (al-i>=1)
+		{
+			c[cl++]+=a[i]*1+a[i+1]*2;
+			i+=2;
+		}
+		else if (al-i>=0)
+		{
+			c[cl++]+=a[i]*1;
+			i+=1;
+		}
+	}
+	cl-=1;
+	printf("hexadecimal:\n");
+	for(i=cl;i>=0;i--)
+	{
+		if(c[i]>=10)
+		{
+			printf("%c",(char)c[i]-10+'A');
+		}
+		else printf("%d",c[i]);
+	}
+	printf("\n");
+}
